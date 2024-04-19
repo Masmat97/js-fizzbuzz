d127e64cd90dcd4ca4bc23fd71666d2d49313f27
Matteo Mascio <matteo.mascio@libero.it>
2024-04-19 15:20:58 +0200

    2

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// for (let i = 1; i < 100; i++) {
-//     console.log(`FOR 1 , i`);
-
-// }
 
 let str = '';
 
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
   str = str + i;
 }
 
